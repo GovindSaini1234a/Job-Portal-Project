72d3a0b94244dead077df40bbcfe81b19b681493
GovindSaini1234a <Govindsaini1234a@gmail.com>
2025-06-10 13:03:19 +0530

    Server Creation

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+
+const app = express();
+
+const PORT =3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})

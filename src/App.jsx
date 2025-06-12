cd33c5f216cad23d5acd7d09ed9d181f4881f5dd
muskan00-dubey <manshidubey71@gmail.com>
2025-06-12 13:41:49 +0530

    frontend by muskan

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import React from 'react'
+import './App.css'
+import Navbar from './components/ui/shared/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
+

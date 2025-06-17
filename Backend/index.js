d6999bdff0fc058e761ab382e87da7d8f91067da
GovindSaini1234a <Govindsaini1234a@gmail.com>
2025-06-17 13:00:08 +0530

    Adding user routes and controllers

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,29 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRouter from "./routes/user.route.js";
 
+dotenv.config({});
 
 const app = express();
 
-const PORT =3000;
+const PORT =process.env.PORT ||3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
 
 
+app.use("api/v1/user",userRouter);
 
 app.listen(PORT,()=>{
     connectDB();
-    console.log(`Server running at port ${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 })
